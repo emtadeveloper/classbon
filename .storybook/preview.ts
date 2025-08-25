bc7a3d492f866b92717cb5f2a89b9c87b6017073
emtadeveloper <emta137671@gmail.com>
2025-08-25 12:30:51 +0330

    1

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,33 +1,15 @@
 // Replace your-framework with the framework you are using (e.g., react, vue3)
 import { Preview } from "@storybook/react";
 
-import { Title, Subtitle, Description, Primary, Controls, Stories } from "@storybook/blocks";
-import React from "react";
 import "../src/app/globals.css";
 
 const preview: Preview = {
   parameters: {
-    doc: {
-      controls: { exclude: ["controls"] },
-    },
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/,
+        date: /Date$/i,
       },
-    },
-    docs: {
-      showPanel: false,
-      // page: () => (
-      //   <>
-      //     <Title />
-      //     <Subtitle />
-      //     <Description />
-      //     <Controls  />
-      //     <Stories />
-      //   </>
-      // ),
     },
   },
 };

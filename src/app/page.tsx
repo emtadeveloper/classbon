e465f76b843ab380ea895285e323531a09289dfd
emtadeveloper <emta137671@gmail.com>
2025-08-25 15:13:19 +0330

    یشسیشسی

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HomeHeroSection from "./_components/home-hero-section/home-hero-section";
 export default function Home() {
   return (
     <>
-      <Colors />
+      {/* <Colors /> */}
       <HomeHeroSection />
     </>
   );
